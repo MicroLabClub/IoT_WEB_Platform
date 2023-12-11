effcbf77dd2153cc8b166d0a84c3f06214959daf
Irina Suhaitchi <irina.suhaitchi@bts.md>
2023-12-11 13:19:16 +0200

    finalizare structura generala a paginii

--- material-react-app/src/layouts/trichoGramma/index.js
+++ material-react-app/src/layouts/trichoGramma/index.js
@@ -10,6 +10,15 @@ import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import { MapContainer, TileLayer } from 'react-leaflet';
 import ReportsLineChart from "../../examples/Charts/LineCharts/ReportsLineChart";
 import reportsLineChartData from "../dashboard/data/reportsLineChartData";
+import * as React from 'react';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+import Box from '@mui/material/Box';
+import Slider from '@mui/material/Slider';
+import MDButton from "components/MDButton";
 
 /*import bogomol from "../../assets/images/insects/bogomol.png";
 import buburuza from "../../assets/images/insects/buburuza.png";
@@ -33,8 +42,35 @@ const TrichoGrammas = () => {
         { id: 7, src: "../../assets/images/insects/omida.png", alt: 'Insect 7' },
         { id: 8, src: "../../assets/images/insects/party.png", alt: 'Insect 8' },
     ];
-
+    
     const { insects } = reportsLineChartData;
+
+    const devices = [
+        'Device 1',
+        'Device 2',
+        'Device 3',
+        'Device 4',
+    ];
+
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const [selectedIndex, setSelectedIndex] = React.useState(0);
+    const open = Boolean(anchorEl);
+    const handleClickListItem = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleMenuItemClick = (event, index) => {
+        setSelectedIndex(index);
+        setAnchorEl(null);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    function valuetext(value) {
+        return `${value} min`;
+    }
 
     return (
         <DashboardLayout marginLeft={274}>
@@ -42,13 +78,13 @@ const TrichoGrammas = () => {
             
             <Grid container spacing={3}>
                 <Grid item xs={12} md={6} lg={6}>
-                    <Card className="mapCard">
+                    <Card>
                         <MDBox display="flex" justifyContent="space-between" alignItems="center" p={3}>
-                            <MDBox>
+                            <div>
                                 <MDTypography variant="h6" gutterBottom>
                                     GPS Tracking
                                 </MDTypography>
-                                <div style={{ width: '550%', height: '500px' }}> {/* Adjusted width */}
+                                <div style={{ width: '550px', height: '500px', flex: 1 }}> {/* Adjusted width */}
                                     <MapContainer
                                         center={defaultPosition}
                                         zoom={18}
@@ -60,6 +96,48 @@ const TrichoGrammas = () => {
                                         />
                                     </MapContainer>
                                 </div>
+                            </div>
+                            <MDBox display="flex" flexDirection="column" alignItems="center" p={3}>
+                                <List
+                                    component="nav"
+                                    aria-label="Device settings"
+                                    sx={{ bgcolor: 'background.paper' }}
+                                >
+                                    <ListItem
+                                    button
+                                    id="lock-button"
+                                    aria-haspopup="listbox"
+                                    aria-controls="lock-menu"
+                                    aria-label="Select device"
+                                    aria-expanded={open ? 'true' : undefined}
+                                    onClick={handleClickListItem}
+                                    >
+                                    <ListItemText
+                                        primary="Select device"
+                                        secondary={devices[selectedIndex]}
+                                    />
+                                    </ListItem>
+                                </List>
+                                <Menu
+                                    id="lock-menu"
+                                    anchorEl={anchorEl}
+                                    open={open}
+                                    onClose={handleClose}
+                                    MenuListProps={{
+                                    'aria-labelledby': 'lock-button',
+                                    role: 'listbox',
+                                    }}
+                                >
+                                    {devices.map((option, index) => (
+                                    <MenuItem
+                                        key={option}
+                                        selected={index === selectedIndex}
+                                        onClick={(event) => handleMenuItemClick(event, index)}
+                                    >
+                                        {option}
+                                    </MenuItem>
+                                    ))}
+                                </Menu>
                             </MDBox>
                         </MDBox>
                     </Card>
@@ -82,30 +160,55 @@ const TrichoGrammas = () => {
                     </Card>
                 </Grid>
             </Grid>
+            
+            <br />
 
             <Grid container spacing={3}>
                 <Grid item xs={12} md={6} lg={6}>
+                    <Card className="nearMapCard" style={{ height: '100%' }}>
+                        <MDBox display="flex" flexDirection="column" alignItems="center" p={3}>
+                            <MDTypography variant="h6" gutterBottom>
+                                Configuration
+                            </MDTypography>
+                            <Box sx={{ width: 300 }}>
+                                <MDTypography variant="caption" gutterBottom>
+                                    Photo Frequency
+                                </MDTypography>
+                                <Slider
+                                    aria-label="Photo Frequency"
+                                    defaultValue={30}
+                                    getAriaValueText={valuetext}
+                                    valueLabelDisplay="auto"
+                                    step={5}
+                                    marks
+                                    min={0}
+                                    max={120}
+                                />
+                            </Box>
+                            <Box>
+                                <MDButton>
+                                    Upload Photo
+                                </MDButton>
+                                <MDButton>
+                                    Delete Photo
+                                </MDButton>
+                            </Box>
+                        </MDBox>
+                    </Card>
                 </Grid>
 
                 <Grid item xs={12} md={6} lg={6}>
+                    <div style={{ paddingTop: '25px', height: '100%' }}>
+                        <ReportsLineChart
+                            color="success"
+                            title="Statistics"
+                            date="updated 4 min ago"
+                            chart={insects}
+                        />
+                    </div>
                 </Grid>
             </Grid>
 
-            <MDBox mt={4.5}>
-                <Grid container spacing={3}>
-                    <Grid item xs={12} md={12} lg={12}>
-                        <MDBox mb={3}>
-                            <ReportsLineChart
-                                color="success"
-                                title="Statistics"
-                                date="updated 4 min ago"
-                                chart={insects}
-                            />
-                        </MDBox>
-                    </Grid>
-
-                </Grid>
-            </MDBox>
             <Footer />
         </DashboardLayout>
     );
