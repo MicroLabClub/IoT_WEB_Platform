09b34109bb90ac3dff563f2af53568f23934ebd7
Irina Suhaitchi <irina.suhaitchi@bts.md>
2023-12-22 13:14:46 +0200

    ajustare UI

--- material-react-app/src/layouts/trichoGramma/index.js
+++ material-react-app/src/layouts/trichoGramma/index.js
@@ -5,95 +5,104 @@ import MDBox from "../../components/MDBox";
 import Grid from "@mui/material/Grid";
 import MDTypography from "../../components/MDTypography";
 import Card from "@mui/material/Card";
-import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import ReportsLineChart from "../../examples/Charts/LineCharts/ReportsLineChart";
 import reportsLineChartData from "../dashboard/data/reportsLineChartData";
-import * as React from "react";
+import React, { useState, useEffect } from "react";
 import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
-import MenuItem from "@mui/material/MenuItem";
-import Menu from "@mui/material/Menu";
+import ListItemButton from "@mui/material/ListItemButton";
 import Box from "@mui/material/Box";
 import Slider from "@mui/material/Slider";
 import MDButton from "components/MDButton";
 
 const TrichoGrammas = () => {
   const defaultPosition = [47.0616, 28.8667];
+  const uploads = "uploads";
+  const procUploads = "proc_uploads";
 
   const insectImages = [
     {
-      id: 1,
-      src: "https://images.theconversation.com/files/497355/original/file-20221125-12-6uo1o4.jpg?ixlib=rb-1.1.0&rect=16%2C0%2C5640%2C3842&q=20&auto=format&w=320&fit=clip&dpr=2&usm=12&cs=strip",
+      index: 1,
+      src: "http://172.17.10.22:5000/uploads/Wed_Dec_13_18:10:32_2023_dev1.jpg",
       alt: "Insect 1",
     },
     {
-      id: 2,
-      src: "https://cdn.britannica.com/22/152822-050-FF5E5F25/Ladybug.jpg",
+      index: 2,
+      src: "http://172.17.10.22:5000/proc_uploads/final_photo_2023-09-28_16-29-55.jpg",
       alt: "Insect 2",
     },
-    {
-      id: 3,
-      src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSY7gqHI2Uvwc7w4p6SHy7646gVs3b0OFQkPg&usqp=CAU",
-      alt: "Insect 3",
-    },
-    {
-      id: 4,
-      src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSchmDUBSoqdQ_C5xBA_W4lmNRzjgjvsi7TDA&usqp=CAU",
-      alt: "Insect 4",
-    },
-    {
-      id: 5,
-      src: "https://media.australian.museum/media/dd/images/b1bc3154262548d0925fc63871845fb2.2e16d0ba.fill-600x400.1a6b084.png",
-      alt: "Insect 5",
-    },
-    {
-      id: 6,
-      src: "https://c02.purpledshub.com/uploads/sites/62/2023/10/What-are-insects.jpg?w=1029&webp=1",
-      alt: "Insect 6",
-    },
-    {
-      id: 7,
-      src: "https://www.nps.gov/cato/learn/nature/images/7-23-17-Browns-Farm-Trail-36-Spicebush-Swallowtail-Caterpillar.jpg?maxwidth=1300&maxheight=1300&autorotate=false",
-      alt: "Insect 7",
-    },
-    {
-      id: 8,
-      src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNUQkMda7hEjeNRaD6EN0Pc7u3ml4MLpUq-Q&usqp=CAU",
-      alt: "Insect 8",
-    },
   ];
+
+  const serverUrl = "http://172.17.10.22:5000";
+  const [images, setImages] = useState([]);
+  const fetchInfo = async () => {
+    const res = await fetch(serverUrl.concat("/collect"));
+    const d = await res.json();
+    return setImages(d);
+  };
+
+  useEffect(() => {
+    fetchInfo();
+  }, []);
+
+  const lists = Object.keys(images).map((key) => {
+    const imagesList = images[key].map((url, index) => ({
+      index: index + 1,
+      url: serverUrl.concat(
+        "/",
+        Array.from(key)[0] === "p"
+          ? procUploads.concat("/", url)
+          : uploads.concat("/", url)
+      ),
+      alt: url,
+    }));
+    return { key, imagesList };
+  });
 
   const { insects } = reportsLineChartData;
 
   const devices = [
-    { name: "Device 1", coordinates: [47.061536, 28.86671] },
-    { name: "Device 2", coordinates: [47.061058, 28.869459] },
-    { name: "Device 3", coordinates: [47.059749, 28.868668] },
-    { name: "Device 4", coordinates: [47.05995, 28.866798] },
+    { index: 1, name: "Device 1", coordinates: [47.061536, 28.86671] },
+    { index: 2, name: "Device 2", coordinates: [47.061058, 28.869459] },
   ];
-
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const [selectedIndex, setSelectedIndex] = React.useState(0);
-  const open = Boolean(anchorEl);
-  const handleClickListItem = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleMenuItemClick = (event, index) => {
-    setSelectedIndex(index);
-    setAnchorEl(null);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
 
   function valuetext(value) {
     return `${value} min`;
   }
+
+  const [selectedDeviceIndex, setSelectedDeviceIndex] = React.useState(0);
+  const [selectedImageIndex, setSelectedImageIndex] = React.useState(0);
+  const [selectedKey, setSelectedKey] = React.useState();
+
+  const handleSelectedKeyChange = (event, key) => {
+    setSelectedKey(key);
+    setSelectedImageIndex(1);
+  };
+
+  const handleDeviceListItemClick = (event, index) => {
+    setSelectedDeviceIndex(index);
+  };
+
+  const handleImageListItemClick = (event, index) => {
+    setSelectedImageIndex(index);
+  };
+
+  const freeIcon = new L.Icon({
+    iconUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png",
+    iconSize: [20, 32],
+    iconAnchor: [16, 32],
+    popupAnchor: [0, -32],
+  });
+
+  const selectedIcon = new L.Icon({
+    iconUrl:
+      "https://cdn4.iconfinder.com/data/icons/small-n-flat/24/map-marker-512.png",
+    iconSize: [32, 32],
+    iconAnchor: [16, 32],
+    popupAnchor: [0, -32],
+  });
 
   return (
     <DashboardLayout marginLeft={274}>
@@ -125,7 +134,14 @@ const TrichoGrammas = () => {
                       attribution="&copy; OpenStreetMap contributors"
                     />
                     {devices.map((device) => (
-                      <Marker position={device.coordinates}>
+                      <Marker
+                        position={device.coordinates}
+                        icon={
+                          device.index === selectedDeviceIndex
+                            ? selectedIcon
+                            : freeIcon
+                        }
+                      >
                         <Popup>{device.name}</Popup>
                       </Marker>
                     ))}
@@ -138,46 +154,59 @@ const TrichoGrammas = () => {
                 alignItems="center"
                 p={3}
               >
+                <MDTypography variant="h6" gutterBottom>
+                  Devices
+                </MDTypography>
                 <List
-                  component="nav"
-                  aria-label="Device settings"
-                  sx={{ bgcolor: "background.paper" }}
-                >
-                  <ListItem
-                    button
-                    id="lock-button"
-                    aria-haspopup="listbox"
-                    aria-controls="lock-menu"
-                    aria-label="Select device"
-                    aria-expanded={open ? "true" : undefined}
-                    onClick={handleClickListItem}
-                  >
-                    <ListItemText
-                      primary="Select device"
-                      secondary={devices[selectedIndex].name}
-                    />
-                  </ListItem>
-                </List>
-                <Menu
-                  id="lock-menu"
-                  anchorEl={anchorEl}
-                  open={open}
-                  onClose={handleClose}
-                  MenuListProps={{
-                    "aria-labelledby": "lock-button",
-                    role: "listbox",
+                  sx={{
+                    width: "100%",
+                    maxWidth: 200,
+                    bgcolor: "background.paper",
+                    position: "relative",
+                    overflow: "auto",
+                    maxHeight: 300,
+                    "& ul": { padding: 0 },
                   }}
                 >
-                  {devices.map((option, index) => (
-                    <MenuItem
-                      key={option.name}
-                      selected={index === selectedIndex}
-                      onClick={(event) => handleMenuItemClick(event, index)}
+                  {devices.map((device) => (
+                    <ListItemButton
+                      selected={selectedDeviceIndex === device.index}
+                      onClick={(event) =>
+                        handleDeviceListItemClick(event, device.index)
+                      }
                     >
-                      {option.name}
-                    </MenuItem>
+                      <ListItemText primary={device.name} />
+                    </ListItemButton>
                   ))}
-                </Menu>
+                </List>
+                <br />
+                <MDTypography variant="h6" gutterBottom>
+                  Image sets
+                </MDTypography>
+                <List
+                  sx={{
+                    width: "100%",
+                    maxWidth: 200,
+                    bgcolor: "background.paper",
+                    position: "relative",
+                    overflow: "auto",
+                    maxHeight: 600,
+                    "& ul": { padding: 0 },
+                  }}
+                >
+                  {lists.map(
+                    (
+                      el // el means element
+                    ) => (
+                      <ListItemButton
+                        selected={selectedKey && selectedKey.key === el.key}
+                        onClick={(event) => handleSelectedKeyChange(event, el)}
+                      >
+                        <ListItemText primary={el.key} />
+                      </ListItemButton>
+                    )
+                  )}
+                </List>
               </MDBox>
             </MDBox>
           </Card>
@@ -194,17 +223,63 @@ const TrichoGrammas = () => {
               <MDTypography variant="h6" gutterBottom>
                 Snapshots
               </MDTypography>
-              <Carousel
-                showArrows={true}
-                infiniteLoop={true}
-                showThumbs={false}
-              >
-                {insectImages.map((image) => (
-                  <div key={image.id}>
-                    <img src={image.src} alt={image.alt} />
-                  </div>
-                ))}
-              </Carousel>
+              <Grid container spacing={2} p={5}>
+                <Grid xs={3} md={3} lg={3} sx={{ maxHeight: 600 }}>
+                  <MDBox
+                    display="flex"
+                    flexDirection="column"
+                    alignItems="center"
+                  >
+                    <List
+                      sx={{
+                        width: "100%",
+                        maxWidth: 220,
+                        bgcolor: "background.paper",
+                        position: "relative",
+                        overflow: "auto",
+                        maxHeight: 520,
+                        "& ul": { padding: 0 },
+                      }}
+                    >
+                      {selectedKey !== undefined ? (
+                        selectedKey.imagesList.map((image) => (
+                          <ListItemButton
+                            selected={selectedImageIndex === image.index}
+                            onClick={(event) =>
+                              handleImageListItemClick(event, image.index)
+                            }
+                          >
+                            <ListItemText primary={image.alt} />
+                          </ListItemButton>
+                        ))
+                      ) : (
+                        <p>No image set selected</p>
+                      )}
+                    </List>
+                  </MDBox>
+                </Grid>
+                <Grid xs={9} md={9} lg={9}>
+                  <MDBox display="flex" p={3} alignItems="center">
+                    {selectedImageIndex && selectedKey ? (
+                      <img
+                        src={
+                          selectedKey.imagesList.find(
+                            (image) => image.index === selectedImageIndex
+                          ).url
+                        }
+                        alt={
+                          selectedKey.imagesList.find(
+                            (image) => image.index === selectedImageIndex
+                          )?.alt || "No alt text available"
+                        }
+                        style={{ maxWidth: "500px", maxHeight: "600px" }}
+                      />
+                    ) : (
+                      <p>No image selected</p>
+                    )}
+                  </MDBox>
+                </Grid>
+              </Grid>
             </MDBox>
           </Card>
         </Grid>
